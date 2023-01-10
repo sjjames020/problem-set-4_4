18b9b08ada1a13b9df3808083293aa9e4104b87d
sjjames020 <sjjames020@gmail.com>
2023-01-10 20:45:33 +0000

    Finished

--- problems/exercises.js
+++ problems/exercises.js
@@ -1,21 +1,81 @@
 // Question 1
-function makeCounter(){
-
+function makeCounter(startingValue){
+  let value = startingValue;
+  function incrementValue(){
+    value ++;
+    console.log(value);
+  }
+return incrementValue()
 }
+
 
 // Question 2
 class Circle {
+  constructor(radius, color){
+    this.radius = radius;
+    this.color = color;
+  } 
+  drawCircle(){
+    return `Drawing a ${this.color} circle.`
+  }
+  getCircumference(){
+    return  2 * Math. PI * this.radius;
+  }
+  getArea(){
+    return (this.radius * this.radius) * Math.PI
+  }
+  changeColor(color){
+    this.color = color
+    return this.color
+  
+  }
 
 }
 
 // Question 3
 class Teacher {
+  constructor(name,school,grade,subject,students){
+    this.name = name;
+    this.school = school;
+    this.grade = grade;
+    this.subject = subject;
+    this.students = []
+  }
+  addStudent(student){
+    this.students.push(student)
+    return this.students.length
+  }
+  changeSchools(school){
+    this.school = school
+    return this.school
+  }
 
 }
 
 // Question 4
 class BankAccount {
- 
+  constructor(firstName, lastName, _balance) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this._balance = 0
+  }
+  showBalance() {
+    return `Your balance is $${this._balance.toFixed(2)}.`
+  }
+  deposit(number) {
+    this._balance = this._balance + number
+    return `Your balance is $${this._balance.toFixed(2)}.`
+  }
+  withdraw(number) {
+    if (number > this._balance) {
+      return `You do not have enough funds.`
+    } else {
+      this._balance = this._balance - number
+      return `Your balance is $${this._balance.toFixed(2)}.`
+    }
+  }
+
+
 }
 
 module.exports = {
--- short-response.md
+++ short-response.md
@@ -3,8 +3,12 @@
 ## Closures, Prototypes, Constructors, and Classes
 
 1. What is a closure?
+ In JavaScript a closure is a function that can access values outside of its own block.
 
 2. What are the benefits of using a constructor functions to create object instances instead of a factory function?
+When using a constructor function each instance can be created with its own unique properties and can inherit methods from that constructors prototype this means changes made to one instance, will not affect other instances.
+
 
 3. What is the `__proto__` property and how does it differ from the `protoype` property?
+Prototype is a hidden property that references an object’s prototype and __proto__ is a method that exposes the hidden Prototype property and allows you to modify it.
 
